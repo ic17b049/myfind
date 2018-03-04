6a7595e338636829a4ba5567915c3b63e90d29e3
Dominic Schebeck <ic17b049@technikum-wien.at>
2018-03-04 21:50:46 +0100

    -ls

--- myfind.c
+++ myfind.c
@@ -1,3 +1,6 @@
+//why the user 160 exists??? first char shouldn't be a number
+//test-find.sh row 491 User hugo do not exist;
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -21,9 +24,7 @@
 
 #include <stdio.h>
 #include <ctype.h>
-//exit(1) unknown option
-//exit(2) expect Option
-//exit(3) expect Param
+
 
 struct optionItem {
 	char *name;
@@ -44,6 +45,9 @@ char *option[] = {
 	};
 
 
+void errorMsg(int i);
+
+uid_t getUidFromString(char *id);
 
 
 int isValidOption(char *option);
@@ -51,21 +55,19 @@ int in_array ( char *needle , char *haystack[], int arraySize);
 struct optionItem *searchOption(char* optName);
 
 void do_dir(const char * dir_name,  char * parms[]);
-void printTest(const char *text);
+
+char **cmdLine;
 
 int main(int argc, char* argv[])
 {	
+	cmdLine = argv;
 
 
 	int expParam = 0;	
 	struct optionItem *optItem = NULL;
 	int optionPos = 0;
-	
-
 
 	for(int i = 0;i<argc;i++){
-
-		
 		switch(i) {
 			case 0:
 				//expect Program name
@@ -77,97 +79,72 @@ int main(int argc, char* argv[])
 				//expect Option/Parameter
 				if(argv[i][0] == '-'){
 					//Option
-					if(expParam > 0) exit(2); // expect Parameter
+					if(expParam > 0) errorMsg(2); // expect Parameter
 					
 					optItem = searchOption(argv[i]);
-					if(optItem == NULL) exit(1); // unknown Option
+					if(optItem == NULL) errorMsg(3); // unknown Option
 					
 					optionPos = i; // Store Option Position
 					expParam = optItem->params;
 					
 				}else{
 					//Parameter
-					if(expParam == 0) exit(3); // Expect Option
+					if(expParam == 0) errorMsg(4); // Expect Option
 					expParam--;
 			
 				}
 				break;	
 		}	
-		
 
-		
 		if(optItem != NULL && optionPos != 0 && expParam == 0){
 			if(strcmp(optItem->name,"-user") == 0){
-				
-				struct passwd *userInfo;
-				
-				//userInfo = getpwnam(argv[(optionPos +1 )]);
-				//if(userInfo == NULL) exit(55);  // unknown User
-				
-				
-				if(isdigit(argv[(optionPos +1 )][0])== 0){
-					long int uid = strtol(argv[(optionPos +1 )], NULL, 10);
-					userInfo = getpwuid(uid);
-					if(userInfo == NULL) exit(55);  // unknown User
-				}else{
-					userInfo = getpwnam(argv[(optionPos +1 )]);
-					if(userInfo == NULL) exit(55);  // unknown User
-				}
-				
-				
-				
-				
-				
-				
-				
+				if(getUidFromString(argv[( optionPos + 1 )]) == 0) errorMsg(5);  // unknown User
 			}
 			
 			if(strcmp(optItem->name,"-type") == 0){
-				char allowedFileTypes[] = "-dbcpls";
-				char *validType = memchr(allowedFileTypes, optionPos +1, strlen(allowedFileTypes));
+				char allowedFileTypes[] = "fdbcpls";
+				char *validType = memchr(allowedFileTypes, argv[( optionPos + 1 )][0], strlen(allowedFileTypes));
 				
-				if(validType == NULL) exit(1);  // unknown User
+				if(validType == NULL) errorMsg(6);  // unknown file type
 			}
 				
 		}
-		
-		
 	}
 	
-	if(expParam > 0) exit(4);  // expect additional option at the end;
-	
-	
-	
+	if(expParam > 0) errorMsg(7);;  // expect additional option at the end;
 	
 	do_dir(argv[1], &argv[2]);
 
 	return 0;
 }
 
-
-
-void printTest(const char *text){
-	char* tmp = (char*)text;
-	printf("_____");
-	while(*tmp != '\0'){
-	printf("%c",*tmp);
-	tmp++;
+void errorMsg(int i){
+	
+	for(i=0;cmdLine[i]!=NULL;i++){
+		fprintf( stderr, "%s ",cmdLine[i]);	
 	}
-	printf("_____\n");
+
+	
+	
+	switch(i) {
+		case 1: fprintf( stderr, "Error: Can not open directory"); exit(i); break;
+		case 2: fprintf( stderr, "Error: expect parameter"); exit(i); break;
+		case 3: fprintf( stderr, "Error: unknown option"); exit(i); break;
+		case 4: fprintf( stderr, "Error: Expect Option"); exit(i); break;
+		case 5: fprintf( stderr, "Error: unknown User"); exit(i); break;
+		case 6: fprintf( stderr, "Error: unknown file type"); exit(i); break;		
+		case 7: fprintf( stderr, "Error: expect additional option at the end"); exit(i); break;
+		case 8: fprintf( stderr, "Error"); exit(i); break;
+		case 9: fprintf( stderr, "Error"); exit(i); break;		
+		
+		
+		
+		default: printf("Another Error"); exit(i); break;
+	}
 }
-
-
-
 
 void do_dir(const char * dir_name,  char * parms[]){
 	
-
-	
-	
-	//printTest(dir_name);
-
-	//char* dirname = "/var/tmp/test-find/simple/";
-	//char* filename = "%*u";
 	char dirname[255] = { '\0' };
 	char filename[255] = { '\0' };
 	const char* tmp = dir_name;
@@ -198,51 +175,65 @@ void do_dir(const char * dir_name,  char * parms[]){
 		j++;
 	}
 
-
 	DIR *currentDirectory =  opendir(dirname);
 
-	if(currentDirectory == NULL) exit(1); // Error Opndir
+	if(currentDirectory == NULL) errorMsg(1); // Error Opndir
 	
 	struct dirent *currentDirEnt = NULL;
 	while((currentDirEnt = readdir(currentDirectory)) != NULL){
 		int print = 1;
 		int found = fnmatch(filename, currentDirEnt->d_name, 0);
-		
+
+		char ganzerPfad[1024] = {'\0'};
+
+		strcpy(ganzerPfad,dirname);
+		strcat(ganzerPfad,currentDirEnt->d_name);
+		int fd = open(ganzerPfad, O_RDONLY);
 		if(parms[0] != NULL){
 			if( strcmp(parms[0],"-user") == 0){
 
-				char ganzerPfad[1024] = {'\n'};
-				
-				
-				struct passwd *userInfo = getpwnam(parms[1]);
-				
-				
-				strcpy(ganzerPfad,dirname);
-				strcat(ganzerPfad,currentDirEnt->d_name);
-				uid_t userid =  userInfo->pw_uid;  
-				
-				int fd = open(ganzerPfad, O_RDONLY);
-				
-
+				uid_t userid = getUidFromString(parms[1]);
 				if(fd >= 0) { 
-					struct stat buffer;
-					fstat(fd, &buffer);
-					if(buffer.st_uid != userid){
+					struct stat fileStat;
+					fstat(fd, &fileStat);
+					if(fileStat.st_uid != userid){
 						print = 0;
 					}
 				}
+			}
+			
+			
+			
+			if( strcmp(parms[0],"-name") == 0){
+				if(fnmatch(parms[1], currentDirEnt->d_name, 0) != 0) print = 0;
+			}
+			
 
+			
+			
+			if( strcmp(parms[0],"-type") == 0){
+				if(fd >= 0) { 
+					struct stat fileStat;
+					fstat(fd, &fileStat);
+					print = 0;
+					switch((int)parms[1][0]) {
+						case 'f': if(S_ISREG(fileStat.st_mode)) print = 1; break;
+						case 'd': if(S_ISDIR(fileStat.st_mode)) print = 1; break;
+						case 'b': if(S_ISBLK(fileStat.st_mode)) print = 1; break;
+						case 'c': if(S_ISCHR(fileStat.st_mode)) print = 1; break;
+						case 'p': if(S_ISFIFO(fileStat.st_mode)) print = 1; break;
+						case 'l': if(S_ISLNK(fileStat.st_mode)) print = 1; break;
+						case 's':  print = 0; break;
+						default: errorMsg(99); break;
+					}
+				}				
 				
 			}
+
+			
+			
 		}
-		
-		
-		
-		
-		
-		
-		
-		
+
 		if(found == 0 && print == 1) {
 			printf("%s%s\n",dirname,currentDirEnt->d_name);
 		}
@@ -291,12 +282,27 @@ struct optionItem *searchOption(char* optName){
 
 int isValidOption(char *chkOption){
 
-	
 	struct optionItem *optItem = searchOption(chkOption);
 	if(optItem != NULL)	return 1;
 	
 	return 0;
 }
 
+uid_t getUidFromString(char *id){
+	struct passwd *userinfo;
+	
+	userinfo = getpwnam(id);
+	if(userinfo != NULL){
+		return userinfo->pw_uid;
+	}
 
+	if(isdigit(id[0]) == 0) return 0;
+	
+	long int userid = strtol(id, NULL, 10);
+	userinfo =  getpwuid(userid);
+	if(userinfo != NULL){
+		return userinfo->pw_uid;;
+	}
+	return 0;
+}
 
